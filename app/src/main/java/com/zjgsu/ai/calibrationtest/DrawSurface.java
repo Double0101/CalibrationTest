48ddfc90714ee87bda74a8e0ea57bb0d17ba77ca
Double0101 <double0101@foxmail.com>
2017-08-01 21:24:20 +0800

    add-gesture

--- app/src/main/java/com/zjgsu/ai/calibrationtest/DrawSurface.java
+++ app/src/main/java/com/zjgsu/ai/calibrationtest/DrawSurface.java
@@ -56,8 +56,8 @@ public class DrawSurface extends SurfaceView {
     private ImageView imageView;
 
     private boolean isTouched = false;
+    private boolean isAjust = false;
     private boolean isMove = false;
-    private boolean isDelete = false;
 
     private Bitmap bitmap;
     private BitmapShader shader;
@@ -65,6 +65,7 @@ public class DrawSurface extends SurfaceView {
 
     private int t = 0;
     private int whichRect = -1;
+    private int whichPoint = -1;
 
     private float multiple;
 
@@ -75,6 +76,7 @@ public class DrawSurface extends SurfaceView {
     private float pY;
 
     private SurfaceHolder holder;
+    private MyGestureDetector detector;
 
     public DrawSurface(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -82,9 +84,11 @@ public class DrawSurface extends SurfaceView {
         setZOrderOnTop(true);
         rects = new ArrayList<MyRectF>();
         holder = getHolder();
+        newRect = null;
         holder.setFormat(PixelFormat.TRANSPARENT);
         holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         initPaint();
+        detector = new MyGestureDetector();
         multiple = 2f;
         magnifierRadius = 250;
         matrix = new Matrix();
@@ -155,7 +159,6 @@ public class DrawSurface extends SurfaceView {
 
     /**
      * 改变放大镜的放大倍数
-     *
      * @param multiple
      */
     public void setMultiple(float multiple) {
@@ -165,10 +168,6 @@ public class DrawSurface extends SurfaceView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        drawMyView(canvas);
-    }
-
-    public void drawMyView(Canvas canvas) {
         if (rects != null) {
             for (int i = 0; i < rects.size(); ++i)
                 canvas.drawRect(rects.get(i), mPaint);
@@ -205,7 +204,6 @@ public class DrawSurface extends SurfaceView {
     /**
      * 通过父类容器调用
      * 得到图片的留白的点的位置
-     *
      * @param bdW
      * @param bdH
      * @param lW
@@ -218,70 +216,123 @@ public class DrawSurface extends SurfaceView {
         bH = lH;
     }
 
-    public int whichMove(float x, float y) {
-        for (int i = 0; i < rects.size(); i++)
-            if (rects.get(i).isPointMove(x, y))
+    /**
+     * 判断触点在哪个框的两个点周围
+     * 随后可以对这个框的大小进行调整
+     * @param x
+     * @param y
+     * @return
+     */
+    public int whichAjsut(float x, float y) {
+        for (int i = 0; i < rects.size(); i++) {
+            whichPoint = rects.get(i).isPointMove(x, y);
+            if (whichPoint != -1)
                 return i;
-        return -1;
-    }
-
-    public int whichDelete(float x, float y) {
-        for (int i = 0; i < rects.size(); i++)
-            if (rects.get(i).isPointDelete(x, y))
-                return i;
-        return -1;
-    }
-
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-        @Override
-        public boolean onSingleTapConfirmed(MotionEvent e) {
-            //  判断触摸到了哪个点
-            if (isDelete) {
-                
-            } else {
-                Toast.makeText(getContext(), "没有选中任何要删除的标注", Toast.LENGTH_SHORT).show();
-            }
-            return super.onSingleTapConfirmed(e);
         }
+        return -1;
+    }
 
+    /**
+     * 判断触点在哪个框的中央
+     * @param x
+     * @param y
+     * @return
+     */
+    public int inWhichCenter(float x, float y) {
+        for (int i = 0; i < rects.size(); i++)
+            if (rects.get(i).isCenter(x, y))
+                return i;
+        return -1;
+    }
+
+    private void clean() {
+        pX = pY = qX = qY = whichRect = whichPoint = -1;
+        newRect = null;
+        isMove = isAjust = isTouched = false;
+    }
+
+    class MyGestureDetector extends GestureDetector.SimpleOnGestureListener {
         @Override
         public boolean onDoubleTap(MotionEvent e) {
-            //  双击事件
+            if (whichRect != -1)
+                rects.remove(whichRect);
+            else {
+                pX = e.getX();
+                pY = e.getY();
+                whichRect = whichAjsut(pX, pY);
+                if (whichRect != -1)
+                    new AlertDialog.Builder(getContext())
+                            .setTitle("删除该标定区域")
+                            .setMessage("您确定要删除该标定吗？")
+                            .setPositiveButton("是", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    rects.remove(whichRect);
+                                    dialog.cancel();
+
+                                }
+                            }).setNegativeButton("否", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    }).show();
+                else
+                    Toast.makeText(getContext(), "您并没有选择任何标注区域", Toast.LENGTH_SHORT).show();
+            }
+            invalidate();
             return super.onDoubleTap(e);
         }
 
         @Override
-        public boolean onDoubleTapEvent(MotionEvent e) {
-            //  双击的过程中触发的事件
-            return super.onDoubleTapEvent(e);
+        public boolean onSingleTapUp(MotionEvent e) {
+            clean();
+            return super.onSingleTapUp(e);
         }
 
         @Override
         public boolean onDown(MotionEvent e) {
             pX = e.getX();
             pY = e.getY();
-            whichRect = whichMove(pX, pY);
-            if (whichRect != -1) {
-                isMove = true;
-            } else {
-                whichRect = whichDelete(pX, pY);
-                isDelete = (whichRect == -1) ? false : true;
+            whichRect = whichAjsut(pX, pY);
+            if (whichRect != -1)
+                isAjust = true;
+            else {
+                whichRect = inWhichCenter(pX, pY);
+                if (whichRect != -1)
+                    isMove = true;
             }
+            newRect = new MyRectF();
             return super.onDown(e);
         }
 
         @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            //  滑动触摸事件
-            if (isMove) {
-
-            } else if (isDelete) {
-
+        public void onShowPress(MotionEvent e) {
+            qX = e.getX();
+            qY = e.getY();
+            if (whichRect != -1) {
+                newRect = null;
+                newRect = rects.get(whichRect);
+                if (isAjust) {
+                    //  移动点
+                    if (whichPoint == 0) {
+                        newRect.left = e.getX();
+                        newRect.top = e.getY();
+                    } else if (whichPoint == 1){
+                        newRect.right = e.getX();
+                        newRect.bottom = e.getY();
+                    }
+                }
+                else if (isMove) {
+                    //  move
+                    newRect.move(pX, pY, qX, qY);
+                }
             } else {
                 //  画框
-
+                newRect.set(pX, pY, qX, qY);
             }
-            return super.onScroll(e1, e2, distanceX, distanceY);
+            invalidate();
+            super.onShowPress(e);
         }
     }
 }
--- app/src/main/java/com/zjgsu/ai/calibrationtest/MyRectF.java
+++ app/src/main/java/com/zjgsu/ai/calibrationtest/MyRectF.java
@@ -7,13 +7,30 @@ import android.graphics.RectF;
  */
 
 public class MyRectF extends RectF {
+
     public MyRectF(float left, float top, float right, float bottom) {
         super(left, top, right, bottom);
     }
-    public boolean isPointDelete(float x, float y) {
-        return Math.abs(x - this.left) < 20 && Math.abs(y - this.top) < 20;
+    public MyRectF() {}
+    public void set(float left, float top, float right, float bottom) {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
     }
-    public boolean isPointMove(float x, float y) {
-        return Math.abs(x - this.right) < 20 && Math.abs(y - this.bottom) < 20;
+    public boolean isCenter(float x, float y) {
+        return Math.abs((right + left) / 2 - x) < width() / 3
+                && Math.abs((bottom - top) / 2 - y) < height() / 3;
+    }
+    public int isPointMove(float x, float y) {
+        return (Math.abs(x - this.right) < 20 && Math.abs(y - this.bottom) < 20) ? 1 :
+                (Math.abs(x - this.left) < 20 && Math.abs(y - this.top) < 20) ? 0 : -1;
+    }
+    public void move(float x1, float y1, float x2, float y2) {
+        float x = x2 - x1, y = y2 - y1;
+        right += x;
+        left += x;
+        top += y;
+        bottom += y;
     }
 }
